8c45fcb2bb14479aadbaaae0b0d5cd830b2811a9
shaun.clay@gmail.com <sclay@sclay-2.local>
2018-08-21 14:12:57 +0200

    Posting with Postman

--- server.js
+++ server.js
@@ -1,12 +1,15 @@
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
+/* handling all the parsing*/
 
-app.post('/',(req.res)=>{
-  var email = req.body.email;
-  var amount = req.body.amount;
+app.use(bodyParser.json());
 
-  res.send({"amount" : amount , "email" : email });
+app.post('/',(req,res)=>{
+ var email = req.body.email;
+ var amount = req.body.amount;
 
+ res.send({"amount" : amount , "email" : email });
 });
 
 app.listen(3000,()=>{
